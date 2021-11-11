7479d3263ae09326a954fffb97f2b1e9719d964e
Jonathan Agusa <54756572+hippiedev@users.noreply.github.com>
2021-11-11 10:56:50 +0100

    UI changes

--- src/Components/Solutions/Solution/Solution.js
+++ src/Components/Solutions/Solution/Solution.js
@@ -6,6 +6,12 @@ import goals from '../../../assets/images/goals.svg';
 import save from '../../../assets/images/save.svg';
 import resume from '../../../assets/images/resume.svg';
 import relax from '../../../assets/images/relax.svg';
+import progress from '../../../assets/images/progress.svg';
+import investing from '../../../assets/images/investing.svg';
+import freelance from '../../../assets/images/freelance.svg';
+import employer from '../../../assets/images/employer.svg';
+import coworker from '../../../assets/images/coworker.svg';
+import dashboard from '../../../assets/images/dashboard.svg';
 
 function Solution(props) {
     let itemImage1, itemImage2, itemImage3;
@@ -33,6 +39,28 @@ function Solution(props) {
             itemContent1 = 'Access Canada’s lowest management fees to save and keep more of your money.';
             itemContent2 = 'We don’t do labels. Your portfolio is uniquely matched based on your investing style and savings goals.';
             itemContent3 = 'Our portfolio managers and smart technology monitors and rebalances your investments. You can sit back and plan your future.';
+            break;
+        case "advisors":
+            itemTitle1 = 'Expand your audience';
+            itemTitle2 = 'Scale your business';
+            itemTitle3 = 'Be independent';
+            itemImage1 = progress;
+            itemImage2 = investing;
+            itemImage3 = freelance;
+            itemContent1 = 'Increase your appeal to high-net-worth and other investors looking to take advantage of a low-cost digital solution.';
+            itemContent2 = 'Save time and money by leveraging technology that reduces time spent on non-revenue generating tasks while keeping more of what you earn.';
+            itemContent3 = 'Take control of the solutions you offer your clients and work with an independent partner that prioritizes the success of you and your client’s best interests.';
+            break;
+        case "employers":
+            itemTitle1 = '100% Free for the Employer';
+            itemTitle2 = 'Digital Onboarding and Setup';
+            itemTitle3 = 'Digital Dashboard';
+            itemImage1 = employer;
+            itemImage2 = coworker;
+            itemImage3 = dashboard;
+            itemContent1 = 'You can help your employees save more at no cost to you.';
+            itemContent2 = '3 Minute Setup – no more paper work or licking envelopes, getting your company setup on Nest Wealth at Work is effortless.';
+            itemContent3 = 'Get the big picture of your entire company plan at your fingertips.';
             break;
         
             default:
--- src/Components/Solutions/Solutions.js
+++ src/Components/Solutions/Solutions.js
@@ -8,10 +8,10 @@ function Solutions() {
     return (
         <div className={styles.Solutions}>
             <div className={styles.SolutionSelection}>
-                <li style={{borderBottom: solution === 'institutions' ? '3px solid rgb(52, 24, 230)' : null}} onClick={() => setSolution('institutions')}>For Financial Institutions</li>
-                <li style={{borderBottom: solution === 'individuals' ? '3px solid #000' : null}} onClick={() => setSolution('individuals')}>For Individual investors</li>
-                <li onClick={() => setSolution('institutions')}>For Financial Advisors</li>
-                <li onClick={() => setSolution('institutions')}>For employers</li>
+                <li style={{borderBottom: solution === 'institutions' ? '4px solid rgb(52, 24, 230)' : null}} onClick={() => setSolution('institutions')}>For Financial Institutions</li>
+                <li style={{borderBottom: solution === 'individuals' ? '4px solid #000' : null}} onClick={() => setSolution('individuals')}>For Individual investors</li>
+                <li style={{borderBottom: solution === 'advisors' ? '4px solid #212F5F' : null}} onClick={() => setSolution('advisors')}>For Financial Advisors</li>
+                <li style={{borderBottom: solution === 'employers' ? '4px solid rgb(80, 80, 80)' : null}} onClick={() => setSolution('employers')}>For employers</li>
             </div>
             <Solution solution={solution} />
         </div>
