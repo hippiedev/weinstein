72ff89519149878df1c9e23cf5752cf8a6e96158
Jonathan Agusa <54756572+hippiedev@users.noreply.github.com>
2021-11-11 09:35:14 +0100

    UI changes

--- src/Components/Solutions/Solutions.js
+++ src/Components/Solutions/Solutions.js
@@ -1,23 +1,9 @@
-import {useState, useEffect} from 'react';
+import {useState} from 'react';
 import Solution from './Solution/Solution';
 import styles from './Solutions.module.css';
 
 function Solutions() {
     const [solution, setSolution] = useState('institutions');
-
-    useEffect(() => {
-        let background = document.querySelector('div.TopSection_TopSection__2PCRL');
-        switch(solution) {
-            case 'institutions':
-                background.style.background = 'rgb(52, 24, 230)';
-                break;
-            case 'individuals':
-                background.style.background = '#000';
-                break; 
-            default:
-                background.style.background = 'rgb(52, 24, 230)';
-        }
-    }, [solution])
 
     return (
         <div className={styles.Solutions}>
