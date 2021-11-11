4e24897bc78699a65acbeca0ab3f7b7bd09033a9
Jonathan Agusa <54756572+hippiedev@users.noreply.github.com>
2021-11-11 08:57:49 +0100

    UI changes

--- src/Components/Solutions/Solution/Solution.js
+++ src/Components/Solutions/Solution/Solution.js
@@ -3,6 +3,9 @@ import styles from './Solution.module.css';
 import growth from '../../../assets/images/growth.svg';
 import productivity from '../../../assets/images/productivity.svg';
 import goals from '../../../assets/images/goals.svg';
+import save from '../../../assets/images/save.svg';
+import resume from '../../../assets/images/resume.svg';
+import relax from '../../../assets/images/relax.svg';
 
 function Solution(props) {
     let itemImage1, itemImage2, itemImage3;
@@ -19,6 +22,17 @@ function Solution(props) {
             itemContent1 = 'Digitize your business processes to reduce errors, account opening times and operational costs while simplifying record-keeping.';
             itemContent2 = 'The My Day Overview provides a holistic view of your client’s activity and insights to help you efficiently serve your clients and proactively prepare for next steps.';
             itemContent3 = 'Build stronger relationships with clients by engaging with them on the things that matter most through a modern, transparent and simple to use portal.';
+            break;
+        case "individuals":
+            itemTitle1 = 'Save more of your money';
+            itemTitle2 = 'Portfolios as unique as you';
+            itemTitle3 = 'Invest & relax';
+            itemImage1 = save;
+            itemImage2 = resume;
+            itemImage3 = relax;
+            itemContent1 = 'Access Canada’s lowest management fees to save and keep more of your money.';
+            itemContent2 = 'We don’t do labels. Your portfolio is uniquely matched based on your investing style and savings goals.';
+            itemContent3 = 'Our portfolio managers and smart technology monitors and rebalances your investments. You can sit back and plan your future.';
             break;
         
             default:
--- src/Components/Solutions/Solutions.js
+++ src/Components/Solutions/Solutions.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import {useState, useEffect} from 'react';
 import Solution from './Solution/Solution';
 import styles from './Solutions.module.css';
 
 function Solutions() {
+    const [solution, setSolution] = useState('institutions');
+
+    useEffect(() => {
+        let background = document.querySelector('div.TopSection_TopSection__2PCRL');
+        switch(solution) {
+            case 'institutions':
+                background.style.background = 'rgb(52, 24, 230)';
+                break;
+            case 'individuals':
+                background.style.background = '#000';
+                break; 
+            default:
+                background.style.background = 'rgb(52, 24, 230)';
+        }
+    }, [solution])
+
     return (
         <div className={styles.Solutions}>
             <div className={styles.SolutionSelection}>
-                <li>For Financial Institutions</li>
-                <li>For Individual investors</li>
-                <li>For Financial Advisors</li>
-                <li>For employers</li>
+                <li style={{borderBottom: solution === 'institutions' ? '3px solid rgb(52, 24, 230)' : null}} onClick={() => setSolution('institutions')}>For Financial Institutions</li>
+                <li style={{borderBottom: solution === 'individuals' ? '3px solid #000' : null}} onClick={() => setSolution('individuals')}>For Individual investors</li>
+                <li onClick={() => setSolution('institutions')}>For Financial Advisors</li>
+                <li onClick={() => setSolution('institutions')}>For employers</li>
             </div>
-            <Solution solution="institutions" />
+            <Solution solution={solution} />
         </div>
     )
 }
